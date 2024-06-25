3f86e969c30a2dcd402500d4d163c863ce988b08
yu <yurimkang20@gmail.com>
2024-06-25 14:56:06 +0900

    upload. [Level 1] 주행거리 비교하기

--- python/Level 1/주행거리 비교하기.py
+++ python/Level 1/주행거리 비교하기.py
@@ -1,2 +1,18 @@
 
 """ Level 1. 주행거리 비교하기 """
+
+import sys
+
+def solution():
+    input = sys.stdin.readline
+
+    A, B = map(int, input().split())
+
+    if A > B:
+        print("A")
+    elif A < B:
+        print("B")
+    else:
+        print("same")
+
+solution()
