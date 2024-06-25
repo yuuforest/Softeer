041340ab267606f3b6c2c5bc16a3ea94fa11f02c
yu <yurimkang20@gmail.com>
2024-06-25 15:17:35 +0900

    upload. [Level 1] 연탄 배달의 시작

--- python/Level 1/연탄 배달의 시작.py
+++ python/Level 1/연탄 배달의 시작.py
@@ -1,2 +1,21 @@
 
 """ Level 1. 연탄 배달의 시작 """
+
+import sys
+
+def solution():
+    input = sys.stdin.readline
+
+    N = int(input())
+    town = list(map(int, input().split()))
+
+    min_diff = town[N-1] - town[0]
+    diff = [0] * (N-1)
+    
+    for idx in range(N-1):
+        diff[idx] = town[idx+1] - town[idx]
+        min_diff = min(min_diff, diff[idx])
+
+    print(diff.count(min_diff))
+    
+solution()
