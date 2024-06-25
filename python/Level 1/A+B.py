d92f8f30989441207ace323c0214a96b3651184d
yu <yurimkang20@gmail.com>
2024-06-25 14:55:55 +0900

    upload. [Level 1] A+B

--- python/Level 1/A+B.py
+++ python/Level 1/A+B.py
@@ -1,2 +1,16 @@
 
 """ Level 1. A + B """
+
+import sys
+
+def solution():
+    input = sys.stdin.readline
+
+    T = int(input())
+    for t in range(T):
+
+        A, B = map(int, input().split())
+
+        print("Case #{0}: {1}".format(t+1, A+B))
+
+solution()
