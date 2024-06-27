4624e9a2f63a8460f3c76ad9af2914fa2369c40e
yu <yurimkang20@gmail.com>
2024-06-27 15:57:12 +0900

    upload. [Level 2] 금고털이

--- python/Level 2/금고털이.py
+++ python/Level 2/금고털이.py
@@ -1,2 +1,32 @@
 
 """ Level 2. 금고털이 """
+
+import sys
+from heapq import heappush, heappop
+
+def solution():
+    input = sys.stdin.readline
+
+    W, N = map(int, input().split()) 
+
+    gold = []
+    for _ in range(N):
+        M, P = map(int, input().split())
+        heappush(gold, (-P, M))
+
+    weight = W
+    answer = 0
+    while weight and gold:
+
+        P, M = heappop(gold)
+
+        if M <= weight:
+            answer += (-P * M)
+            weight -= M
+        else:
+            answer += (-P * weight)
+            weight = 0
+
+    print(answer)
+        
+solution()
