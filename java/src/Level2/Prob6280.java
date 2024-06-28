9cf3602efef082af4fe5f1b0d82672cde4ca91e5
yu <yurimkang20@gmail.com>
2024-06-29 01:14:50 +0900

    upload. [Level 2] 6280. 지도 자동 구축

--- java/src/Level2/Prob6280.java
+++ java/src/Level2/Prob6280.java
@@ -1,5 +1,23 @@
 package Level2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Prob6280 {
     
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int answer = 2;
+        for (int i = 0; i < N; i++) {
+            answer += (answer-1);
+        }
+
+        System.out.println(answer * answer);
+        // System.out.println(Math.pow(answer, 2));
+    }
 }
