2987c2c45bc45930ab5bacb39ec9263ea7b03c8f
yu <yurimkang20@gmail.com>
2024-06-28 14:53:54 +0900

    upload. [Level 1] 6295. A+B

--- java/src/Level1/Prob6295.java
+++ java/src/Level1/Prob6295.java
@@ -1,5 +1,23 @@
 package Level1;
 
+import java.io.*;
+import java.util.*;
+
 public class Prob6295 {
     
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        int A, B;
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            A = Integer.parseInt(st.nextToken());
+            B = Integer.parseInt(st.nextToken());
+            System.out.printf("Case #%d: %d\n", (i+1), (A+B));
+        }
+    }
 }
