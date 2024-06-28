9a0e7dcc84a8c2ea3503b6609db08b47e5eb2f87
yu <yurimkang20@gmail.com>
2024-06-28 14:54:54 +0900

    upload. [Level 1] 7695. [한양대 HCPC 2023] Tren del Fin del Mundo

--- java/src/Level1/Prob7695.java
+++ java/src/Level1/Prob7695.java
@@ -1,5 +1,29 @@
 package Level1;
 
+import java.io.*;
+import java.util.*;
+
 public class Prob7695 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int ansx = 0, ansy = 1001;
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            if (y < ansy) {
+                ansx = x;
+                ansy = y;
+            }
+        }
+
+        System.out.printf("%d %d", ansx, ansy);
+    }
     
 }
