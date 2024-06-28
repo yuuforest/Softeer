ee4e296ef79a7cc4c5676dae6c94efc9499e3c4d
yu <yurimkang20@gmail.com>
2024-06-28 14:54:02 +0900

    upload. [Level 1] 6254. 근무 시간

--- java/src/Level1/Prob6254.java
+++ java/src/Level1/Prob6254.java
@@ -1,5 +1,25 @@
 package Level1;
 
+import java.io.*;
+import java.util.*;
+
 public class Prob6254 {
     
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int answer = 0;
+        StringTokenizer st;
+        for (int i = 0; i < 5; i++) {
+            st = new StringTokenizer(br.readLine());
+            String[] t1 = st.nextToken().split(":");
+            String[] t2 = st.nextToken().split(":");
+
+            answer += ((Integer.parseInt(t2[0]) * 60 + Integer.parseInt(t2[1])) 
+                  - (Integer.parseInt(t1[0]) * 60 + Integer.parseInt(t1[1])));
+        }
+
+        System.out.println(answer);
+    }
 }
