68483820cb68cabb3c5b2f76ea23728e63cd2092
yu <yurimkang20@gmail.com>
2024-06-28 14:55:02 +0900

    upload. [Level 1] 7698. 개표

--- java/src/Level1/Prob7698.java
+++ java/src/Level1/Prob7698.java
@@ -1,5 +1,28 @@
 package Level1;
 
+import java.io.*;
+
 public class Prob7698 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            int N = Integer.parseInt(br.readLine());
+
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < (N / 5); j++) {
+                sb.append("++++ ");
+            }
+
+            for (int j = 0; j < (N % 5); j++) {
+                sb.append("|");
+            }
+            
+            System.out.println(sb.toString());
+        }
+    }
     
 }
