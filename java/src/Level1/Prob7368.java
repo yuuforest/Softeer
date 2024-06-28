4637352dcf035812183db206d6dae74b5f85b4cc
yu <yurimkang20@gmail.com>
2024-06-28 14:54:34 +0900

    upload. [Level 1] 7368. 위험한 효도

--- java/src/Level1/Prob7368.java
+++ java/src/Level1/Prob7368.java
@@ -1,5 +1,26 @@
 package Level1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Prob7368 {
     
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+
+        int answer = 0;
+
+        answer += ((D / A) * (A+B)) + ((D % A) == 0 ? -B : (D % A));
+        answer += ((D / B) * (A+B)) + ((D % B) == 0 ? -A : (D % B));
+
+        System.out.println(answer);
+    }
 }
