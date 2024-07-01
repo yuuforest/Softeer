41d01ffe583af708c6166a48f5f97ca453bbb4c3
yu <yurimkang20@gmail.com>
2024-07-01 14:38:00 +0900

    upload. [Level 2] 바이러스

--- python/Level 2/바이러스.py
+++ python/Level 2/바이러스.py
@@ -1,2 +1,25 @@
 
 """ Level 2. 바이러스 """
+
+import sys
+
+def solution():
+    input = sys.stdin.readline
+
+    K, P, N = map(int, input().split())     # 처음 바이러스의 수 K, 증가율 P, 총 시간 N
+
+    print(K * spread(P, N) % 1000000007)
+
+def spread(p:int, n:int):
+
+    if (n == 1):
+        return p
+
+    temp = spread(p, n//2)
+
+    if (n % 2 == 0):
+        return temp * temp % 1000000007
+    else:
+        return temp * temp * p % 1000000007
+
+solution()
